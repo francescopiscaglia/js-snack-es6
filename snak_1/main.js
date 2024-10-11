85f7a26109043e5ff06c81ddc42b83fe4fc9f846
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-11 15:32:06 +0200

    add markup

--- snak_1/main.js
+++ snak_1/main.js
@@ -26,15 +26,34 @@ const bikes = [
 
 let arr = [];
 let lowestWeightBike;
+
+// seleziono gli elementi dalla DOM
+const bikeSpecsEL = document.querySelector(".list-group");
+const lowestWeightEL = document.getElementById("lowest");
+
 // accedere agli elementi dell'array
 for (let i  = 0; i < bikes.length; i++) {
     const singleBike = bikes[i]; // bici singola
     
-    const bikeWeight = singleBike.weight;
-    arr.push(bikeWeight);
-}
-console.log(arr);
-console.log(Math.min(...arr));
+    const bikeWeight = singleBike.weight; // salvo le proprietà weight di ogni bici 
+    arr.push(bikeWeight); // pusho il valore di weight nell'array vuoto
+
+    // costruisco il markup da aggiungere
+    const markup = `
+        <li class="list-group-item">
+            ${singleBike.name}: ${singleBike.weight} kg
+        </li>
+    `;
+
+    // inserisco il markup
+    bikeSpecsEL.insertAdjacentHTML("beforeend", markup);
+};
+
+// ottengo il valore più piccolo dell'array
+lowestWeightBike = Math.min(...arr);
+
+// stampo il risultato
+lowestWeightEL.innerHTML = `La più leggera è di ${lowestWeightBike} kg`;
 
 
-// accedere alla proprietà weight dell'oggetto
+
