e05925c7f29b1c3263c36a2961df462a53e7a9ed
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-11 14:38:35 +0200

    add object array

---
+++ snak_1/main.js
@@ -0,0 +1,27 @@
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+
+const bikes = [
+    {
+        name: "BMX",
+        weight: 10,
+    },
+    {
+        name: "Road-bike",
+        weight: 10,
+    },
+    {
+        name: "Offroad-bike",
+        weight: 10,
+    },
+    {
+        name: "Mountainbike",
+        weight: 10,
+    },
+];
+
+console.log(bikes);
